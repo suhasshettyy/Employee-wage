ae52da8093876e4bed076cef6e9ed390a5c3c767
suhasshettyy <scshetty07@gmail.com>
2021-07-08 17:22:46 +0530

    added: UC5-empMonthwage

--- empwage.sh
+++ empwage.sh
@@ -3,11 +3,12 @@ emp=$((RANDOM%3))
 wage=20
 hr=8
 partime=8
+days=20
 case "$emp" in
-1) (( wage = $wage * $hr )) 
+1) (( wage = ($wage * $hr) * $days )) 
 echo "Employee wage is $wage"
 ;;
-2) (( wage = $wage * $partime ))
+2) (( wage = ($wage * $partime) * $days ))
 echo "Employee Part Time Wage is $wage"
 ;;
 3) echo "Employee is absent"
