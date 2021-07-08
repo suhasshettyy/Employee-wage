4a3388d94be489721b352cc35324664287164c60
suhasshettyy <scshetty07@gmail.com>
2021-07-08 17:16:44 +0530

    added: UC4-empCasewage

--- empwage.sh
+++ empwage.sh
@@ -3,11 +3,15 @@ emp=$((RANDOM%3))
 wage=20
 hr=8
 partime=8
-if [ $emp -eq 1 ]
-then (( wage = $wage * $hr )) 
+case "$emp" in
+1) (( wage = $wage * $hr )) 
 echo "Employee wage is $wage"
-elif [ $emp -eq 2 ]
-then (( wage = $wage * $partime ))
+;;
+2) (( wage = $wage * $partime ))
 echo "Employee Part Time Wage is $wage"
-else echo "Employee is absent"
-fi
+;;
+3) echo "Employee is absent"
+;;
+*) echo "invalid"
+;;
+esac
