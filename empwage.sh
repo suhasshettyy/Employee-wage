403145bb9884bcb396c16ec106470f93f163a670
suhasshettyy <scshetty07@gmail.com>
2021-07-08 17:08:17 +0530

    added: UC3-empPartimewage

--- empwage.sh
+++ empwage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
-emp=$((RANDOM%2))
+emp=$((RANDOM%3))
 wage=20
 hr=8
+partime=8
 if [ $emp -eq 1 ]
 then (( wage = $wage * $hr )) 
 echo "Employee wage is $wage"
+elif [ $emp -eq 2 ]
+then (( wage = $wage * $partime ))
+echo "Employee Part Time Wage is $wage"
 else echo "Employee is absent"
 fi
