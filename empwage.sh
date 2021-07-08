404ec934c1bf38daf1529b9cc8c3c18517e5e371
suhasshettyy <scshetty07@gmail.com>
2021-07-08 18:54:24 +0530

    added: UC7-empCode

--- empwage.sh
+++ empwage.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -x
 fulltime=1
 partime=2
-empcheck=$((RANDOM%3))
 wageperhr=20
 dayspermonth=20
 maxhrsinmonth=100
 totalemphr=0
 totalworkingdays=0
-while [[ $totalemphr -lt $maxhrsinmonth && $totalworkingdays -lt $dayspermonth ]]
-do 
-((totalworkingdays++))
-case "$empcheck" in
+
+function workinghours
+{
+case "$1" in
 $fulltime)
 emphrs=8
 ;;
@@ -21,6 +20,12 @@ emphrs=8
 emphrs=0
 ;;
 esac
+echo $emphrs
+}
+while [[ $totalemphr -lt $maxhrsinmonth && $totalworkingdays -lt $dayspermonth ]]
+do
+((totalworkingdays++))
+emphrs="$( workinghours $((RANDOM%3)) )"
 totalemphr=$((totalemphr+emphrs))
 done
 totalsalary=$((totalemphr*wageperhr))
